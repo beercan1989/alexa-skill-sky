383dd4bd5e4089fa7590a2645dbf79a22957d8ca
James Bacon <james@baconi.co.uk>
2020-05-19 13:45:46 +0100

    Cleaned up the discovery code.

--- src/main/kotlin/uk/co/baconi/alexa/skill/sky/discovery/DiscoveryService.kt
+++ src/main/kotlin/uk/co/baconi/alexa/skill/sky/discovery/DiscoveryService.kt
@@ -1,28 +1,13 @@
 package uk.co.baconi.alexa.skill.sky.discovery
 
 import arrow.fx.IO
-import arrow.fx.IO.Companion.effect
-import arrow.fx.extensions.fx
 import arrow.fx.extensions.io.concurrent.parTraverse
-import arrow.fx.extensions.io.monad.map
 import com.github.maltalex.ineter.base.IPv4Address
 import com.github.maltalex.ineter.range.IPv4Range
 import com.typesafe.config.Config
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Executors
-
-suspend fun threadName(): String =
-    Thread.currentThread().name
-
-data class ThreadInfo(
-    val threadA: String,
-    val threadB: String,
-    val threadC: String
-)
 
 class DiscoveryService(private val iPv4Range: IPv4Range, private val portScanner: PortScanner) {
 
@@ -39,16 +24,16 @@ class DiscoveryService(private val iPv4Range: IPv4Range, private val portScanner
 
     fun discoverDevices(): IO<List<Device>> {
         val devices = iPv4Range.parTraverse(coroutineDispatcher) { iPv4Address ->
-            effect { scanAddress(iPv4Address) }
+            scanAddress(iPv4Address)
         }
         return devices.map { list ->
             list.filterNotNull()
         }
     }
 
-    private fun scanAddress(iPv4Address: IPv4Address): Device? {
+    private fun scanAddress(iPv4Address: IPv4Address): IO<Device?> = IO.effect {
         logger.trace("Scanning {}", iPv4Address)
-        return when {
+        when {
             // hasRestInterfacePortOpen
             portScanner.isPortOpen(iPv4Address, 9006) -> {
                 when {
